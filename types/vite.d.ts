e33e21bb560d403249299e88a69cf9e5a2ce25e4
Lucas Porfirio Dev <lucasporfirioa@gmail.com>
2025-10-31 01:12:47 -0300

    chore: create vite.d.ts to extend vite test config typing

---
+++ types/vite.d.ts
@@ -0,0 +1,8 @@
+import "vite";
+import type { UserConfig as VitestUserConfig } from "vitest/config";
+
+declare module "vite" {
+  interface UserConfig {
+    test?: VitestUserConfig["test"];
+  }
+}
